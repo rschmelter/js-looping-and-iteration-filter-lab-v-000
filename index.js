f477090e348bf7cc5226351fb0a0faed3f949efe
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-11 17:39:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
-    const array = drivers.filter(function (driver) {driver.name.toLowerCase() === name.toLowerCase()});
+    const array = drivers.filter(function (driver) {driver.toLowerCase() === name.toLowerCase()});
     return array
 
 }
